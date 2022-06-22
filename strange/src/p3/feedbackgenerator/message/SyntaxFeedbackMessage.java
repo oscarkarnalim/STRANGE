68fa42cc243d7231c77bef2696af443eee336c58
oscarkarnalim <56093833+oscarkarnalim@users.noreply.github.com>
2022-06-23 08:30:04 +1000

    Minor fix

--- strange/src/p3/feedbackgenerator/message/SyntaxFeedbackMessage.java
+++ strange/src/p3/feedbackgenerator/message/SyntaxFeedbackMessage.java
@@ -14,10 +14,9 @@ public class SyntaxFeedbackMessage extends MultipleFeedbackMessage {
 	protected String humanLanguageCode;
 	protected boolean isExplanationDetailed;
 
-	public SyntaxFeedbackMessage(String action, String disguiseTarget,
-			ArrayList<FeedbackToken> tokenList1,
-			ArrayList<FeedbackToken> tokenList2,
-			boolean areWhitespacesModified, String humanLanguageCode, boolean isExplanationDetailed) {
+	public SyntaxFeedbackMessage(String action, String disguiseTarget, ArrayList<FeedbackToken> tokenList1,
+			ArrayList<FeedbackToken> tokenList2, boolean areWhitespacesModified, String humanLanguageCode,
+			boolean isExplanationDetailed) {
 		super(action, disguiseTarget, tokenList1, tokenList2);
 		// TODO Auto-generated constructor stub
 		this.updateActionAndGenerateAdditionalDetails();
@@ -39,12 +38,11 @@ public class SyntaxFeedbackMessage extends MultipleFeedbackMessage {
 				this.action = "modified";
 
 				/*
-				 * create additional message and add it. target disguise is
-				 * described based on given token content for comparison.
+				 * create additional message and add it. target disguise is described based on
+				 * given token content for comparison.
 				 */
 				String disguiseTarget = cur1.getContentForComparison();
-				int index = MismatchSyntaxMessage.indexOfAList(
-						mismatchSyntaxMessages, disguiseTarget,
+				int index = MismatchSyntaxMessage.indexOfAList(mismatchSyntaxMessages, disguiseTarget,
 						cur1.getContent(), cur2.getContent());
 
 				// check if such a message exists on the list
@@ -59,9 +57,7 @@ public class SyntaxFeedbackMessage extends MultipleFeedbackMessage {
 				}
 
 				// add the message
-				msu.getMismatchTokenMessages().add(
-						new StandardFeedbackMessage("modified", disguiseTarget,
-								cur1, cur2));
+				msu.getMismatchTokenMessages().add(new StandardFeedbackMessage("modified", disguiseTarget, cur1, cur2));
 			}
 		}
 
@@ -148,29 +144,22 @@ public class SyntaxFeedbackMessage extends MultipleFeedbackMessage {
 	}
 
 	private String getDetailedSyntaxDifferenceMessageEn() {
-		// if detailed explanation is not expected, return empty string		
-		if(isExplanationDetailed == false)
+		// if detailed explanation is not expected, return empty string
+		if (isExplanationDetailed == false)
 			return "";
-		
+
 		String s = "";
 		s += "<br /><br /><b>The captured differences:</b><ol>";
 		for (int i = 0; i < mismatchSyntaxMessages.size(); i++) {
-			ArrayList<StandardFeedbackMessage> messages = mismatchSyntaxMessages
-					.get(i).getMismatchTokenMessages();
+			ArrayList<StandardFeedbackMessage> messages = mismatchSyntaxMessages.get(i).getMismatchTokenMessages();
 			StandardFeedbackMessage cur = messages.get(0);
-			int occurrence = mismatchSyntaxMessages.get(i)
-					.getMismatchTokenMessages().size();
+			int occurrence = mismatchSyntaxMessages.get(i).getMismatchTokenMessages().size();
 			if (cur.getAction().equals("modified")) {
-				s += "\n\t<li> "
-						+ (cur.getDisguiseTarget().charAt(0) + "")
-								.toUpperCase()
-						+ cur.getDisguiseTarget().substring(1)
-						+ " <span class=\"syntaxsim\">"
-						+ JavaHtmlGenerator.HTMLSafeStringFormatWithSpace(cur
-								.getToken1().getContent())
+				s += "\n\t<li> " + (cur.getDisguiseTarget().charAt(0) + "").toUpperCase()
+						+ cur.getDisguiseTarget().substring(1) + " <span class=\"syntaxsim\">"
+						+ JavaHtmlGenerator.HTMLSafeStringFormatWithSpace(cur.getToken1().getContent())
 						+ "</span> is changed to <span class=\"syntaxsim\">"
-						+ JavaHtmlGenerator.HTMLSafeStringFormatWithSpace(cur
-								.getToken2().getContent())
+						+ JavaHtmlGenerator.HTMLSafeStringFormatWithSpace(cur.getToken2().getContent())
 						+ "</span> and used ";
 				if (occurrence > 1)
 					s += occurrence + " times.";
@@ -184,34 +173,30 @@ public class SyntaxFeedbackMessage extends MultipleFeedbackMessage {
 
 		// explaining how the aligning results
 		s += "<b>Aligned contents (with differences highlighted):</b> <br /> <br />";
-		s += "<table><tr class=\"inexplanation\">"
-				+ "<th class=\"inexplanation\">Aligned position</th>"
-				+ "<th class=\"inexplanation\">Left form</th>"
-				+ "<th class=\"inexplanation\">Left position</th>"
-				+ "<th class=\"inexplanation\">Right form</th>"
-				+ "<th class=\"inexplanation\">Right position</th>" + "</tr>";
+		s += "<table><tr class=\"inexplanation\">" + "<th class=\"inexplanation\">Aligned position</th>"
+				+ "<th class=\"inexplanation\">Left form</th>" + "<th class=\"inexplanation\">Left position</th>"
+				+ "<th class=\"inexplanation\">Right form</th>" + "<th class=\"inexplanation\">Right position</th>"
+				+ "</tr>";
 		for (int k = 0; k < tokenList1.size(); k++) {
 			// get w1 info
 			String w1 = tokenList1.get(k).getContent();
 			/*
-			 * if the comparison token is different to the raw content, add some
-			 * info explaining the token for comparison.
+			 * if the comparison token is different to the raw content, add some info
+			 * explaining the token for comparison.
 			 */
 			if (w1.equals(tokenList1.get(k).getContentForComparison()) == false)
 				w1 += " (" + tokenList1.get(k).getContentForComparison() + ")";
-			String pos1 = "line " + tokenList1.get(k).getStartRow()
-					+ " column " + tokenList1.get(k).getStartCol();
+			String pos1 = "line " + tokenList1.get(k).getStartRow() + " column " + tokenList1.get(k).getStartCol();
 
 			// get w2 info
 			String w2 = tokenList2.get(k).getContent();
 			/*
-			 * if the comparison token is different to the raw content, add some
-			 * info explaining the token for comparison.
+			 * if the comparison token is different to the raw content, add some info
+			 * explaining the token for comparison.
 			 */
 			if (w2.equals(tokenList2.get(k).getContentForComparison()) == false)
 				w2 += " (" + tokenList2.get(k).getContentForComparison() + ")";
-			String pos2 = "line " + tokenList2.get(k).getStartRow()
-					+ " column " + tokenList2.get(k).getStartCol();
+			String pos2 = "line " + tokenList2.get(k).getStartRow() + " column " + tokenList2.get(k).getStartCol();
 
 			s += "<tr class=\"inexplanation\">";
 			// print a number, just for referencing clarity
@@ -219,8 +204,7 @@ public class SyntaxFeedbackMessage extends MultipleFeedbackMessage {
 
 			// write first token
 			if (w1.equals(w2) == false) {
-				s += "<td class=\"inexplanation\"><span class=\"syntaxsim\">"
-						+ w1 + "</span></td>";
+				s += "<td class=\"inexplanation\"><span class=\"syntaxsim\">" + w1 + "</span></td>";
 			} else {
 				s += "<td class=\"inexplanation\">" + w1 + "</td>";
 			}
@@ -228,8 +212,7 @@ public class SyntaxFeedbackMessage extends MultipleFeedbackMessage {
 
 			// write second token
 			if (w1.equals(w2) == false) {
-				s += "<td class=\"inexplanation\"><span class=\"syntaxsim\">"
-						+ w2 + "</span></td>";
+				s += "<td class=\"inexplanation\"><span class=\"syntaxsim\">" + w2 + "</span></td>";
 			} else {
 				s += "<td class=\"inexplanation\">" + w2 + "</td>";
 			}
@@ -250,8 +233,8 @@ public class SyntaxFeedbackMessage extends MultipleFeedbackMessage {
 					s = "<b>Kedua fragmen kode memiliki tata letak yang berbeda namun berisi konten yang sama.</b> ";
 					s += "Jika tugas yang diberikan memiliki banyak alternatif solusi dan para murid tidak wajib menggunakan konten fragmen ini, hal ini mencurigakan. "
 							+ "Pada keadaan tersebut sangatlah langka dua atau lebih murid jujur menggunakan implementasi solusi yang sama. ";
-					s += "Hal ini juga dapat mencurigakan jika paling tidak salah satu fragmen memiliki tata letak yang tidak umum; ketidakumuman tersebut bisa jadi " +
-							"salah satu percobaan untuk mengkaburkan kesamaan.";
+					s += "Hal ini juga dapat mencurigakan jika paling tidak salah satu fragmen memiliki tata letak yang tidak umum; ketidakumuman tersebut bisa jadi "
+							+ "salah satu percobaan untuk mengkaburkan kesamaan.";
 				} else {
 					// copied with the same white space
 					s = "<b>Kedua fragmen kode identik satu sama lain.</b> ";
@@ -278,8 +261,8 @@ public class SyntaxFeedbackMessage extends MultipleFeedbackMessage {
 					s = "<b>Kedua fragmen kode sama walaupun ada beberapa perbedaan pada konten dan tata letak.</b> ";
 					s += "Jika tugas yang diberikan memiliki banyak alternatif solusi dan para murid tidak wajib menggunakan konten fragmen ini, hal ini mencurigakan. "
 							+ "Pada keadaan tersebut sangatlah langka dua atau lebih murid jujur menggunakan implementasi solusi yang sama. ";
-					s += "Hal ini juga dapat mencurigakan jika paling tidak salah satu fragmen memiliki tata letak yang tidak umum; ketidakumuman tersebut bisa jadi " +
-							"salah satu percobaan untuk mengkaburkan kesamaan.";
+					s += "Hal ini juga dapat mencurigakan jika paling tidak salah satu fragmen memiliki tata letak yang tidak umum; ketidakumuman tersebut bisa jadi "
+							+ "salah satu percobaan untuk mengkaburkan kesamaan.";
 					s += getDetailedSyntaxDifferenceMessageId();
 				} else {
 					// modified with the same white space
@@ -308,26 +291,22 @@ public class SyntaxFeedbackMessage extends MultipleFeedbackMessage {
 	}
 
 	private String getDetailedSyntaxDifferenceMessageId() {
+		// if detailed explanation is not expected, return empty string
+		if (isExplanationDetailed == false)
+			return "";
+
 		String s = "";
 		s += "<br /><br /><b>Aspek pembeda konten yang tertangkap:</b><ol>";
 		for (int i = 0; i < mismatchSyntaxMessages.size(); i++) {
-			ArrayList<StandardFeedbackMessage> messages = mismatchSyntaxMessages
-					.get(i).getMismatchTokenMessages();
+			ArrayList<StandardFeedbackMessage> messages = mismatchSyntaxMessages.get(i).getMismatchTokenMessages();
 			StandardFeedbackMessage cur = messages.get(0);
-			int occurrence = mismatchSyntaxMessages.get(i)
-					.getMismatchTokenMessages().size();
+			int occurrence = mismatchSyntaxMessages.get(i).getMismatchTokenMessages().size();
 			if (cur.getAction().equals("modified")) {
-				s += "\n\t<li> "
-						+ (cur.getDisguiseTarget().charAt(0) + "")
-								.toUpperCase()
-						+ cur.getDisguiseTarget().substring(1)
-						+ " <span class=\"syntaxsim\">"
-						+ JavaHtmlGenerator.HTMLSafeStringFormatWithSpace(cur
-								.getToken1().getContent())
+				s += "\n\t<li> " + (cur.getDisguiseTarget().charAt(0) + "").toUpperCase()
+						+ cur.getDisguiseTarget().substring(1) + " <span class=\"syntaxsim\">"
+						+ JavaHtmlGenerator.HTMLSafeStringFormatWithSpace(cur.getToken1().getContent())
 						+ "</span> diganti dengan <span class=\"syntaxsim\">"
-						+ JavaHtmlGenerator.HTMLSafeStringFormatWithSpace(cur
-								.getToken2().getContent())
-						+ "</span> ";
+						+ JavaHtmlGenerator.HTMLSafeStringFormatWithSpace(cur.getToken2().getContent()) + "</span> ";
 				if (occurrence > 1)
 					s += "sebanyak " + occurrence + " kali.";
 				else
@@ -340,34 +319,30 @@ public class SyntaxFeedbackMessage extends MultipleFeedbackMessage {
 
 		// explaining how the aligning results
 		s += "<b>Konten pada fragmen kode yang sudah diratakan (dengan aspek perbedaan ditandai):</b> <br /> <br />";
-		s += "<table><tr class=\"inexplanation\">"
-				+ "<th class=\"inexplanation\">Posisi perataan</th>"
-				+ "<th class=\"inexplanation\">Bentuk kiri</th>"
-				+ "<th class=\"inexplanation\">Posisi kiri</th>"
-				+ "<th class=\"inexplanation\">Bentuk kanan</th>"
-				+ "<th class=\"inexplanation\">Posisi kanan</th>" + "</tr>";
+		s += "<table><tr class=\"inexplanation\">" + "<th class=\"inexplanation\">Posisi perataan</th>"
+				+ "<th class=\"inexplanation\">Bentuk kiri</th>" + "<th class=\"inexplanation\">Posisi kiri</th>"
+				+ "<th class=\"inexplanation\">Bentuk kanan</th>" + "<th class=\"inexplanation\">Posisi kanan</th>"
+				+ "</tr>";
 		for (int k = 0; k < tokenList1.size(); k++) {
 			// get w1 info
 			String w1 = tokenList1.get(k).getContent();
 			/*
-			 * if the comparison token is different to the raw content, add some
-			 * info explaining the token for comparison.
+			 * if the comparison token is different to the raw content, add some info
+			 * explaining the token for comparison.
 			 */
 			if (w1.equals(tokenList1.get(k).getContentForComparison()) == false)
 				w1 += " (" + tokenList1.get(k).getContentForComparison() + ")";
-			String pos1 = "line " + tokenList1.get(k).getStartRow()
-					+ " column " + tokenList1.get(k).getStartCol();
+			String pos1 = "line " + tokenList1.get(k).getStartRow() + " column " + tokenList1.get(k).getStartCol();
 
 			// get w2 info
 			String w2 = tokenList2.get(k).getContent();
 			/*
-			 * if the comparison token is different to the raw content, add some
-			 * info explaining the token for comparison.
+			 * if the comparison token is different to the raw content, add some info
+			 * explaining the token for comparison.
 			 */
 			if (w2.equals(tokenList2.get(k).getContentForComparison()) == false)
 				w2 += " (" + tokenList2.get(k).getContentForComparison() + ")";
-			String pos2 = "line " + tokenList2.get(k).getStartRow()
-					+ " column " + tokenList2.get(k).getStartCol();
+			String pos2 = "line " + tokenList2.get(k).getStartRow() + " column " + tokenList2.get(k).getStartCol();
 
 			s += "<tr class=\"inexplanation\">";
 			// print a number, just for referencing clarity
@@ -375,8 +350,7 @@ public class SyntaxFeedbackMessage extends MultipleFeedbackMessage {
 
 			// write first token
 			if (w1.equals(w2) == false) {
-				s += "<td class=\"inexplanation\"><span class=\"syntaxsim\">"
-						+ w1 + "</span></td>";
+				s += "<td class=\"inexplanation\"><span class=\"syntaxsim\">" + w1 + "</span></td>";
 			} else {
 				s += "<td class=\"inexplanation\">" + w1 + "</td>";
 			}
@@ -384,8 +358,7 @@ public class SyntaxFeedbackMessage extends MultipleFeedbackMessage {
 
 			// write second token
 			if (w1.equals(w2) == false) {
-				s += "<td class=\"inexplanation\"><span class=\"syntaxsim\">"
-						+ w2 + "</span></td>";
+				s += "<td class=\"inexplanation\"><span class=\"syntaxsim\">" + w2 + "</span></td>";
 			} else {
 				s += "<td class=\"inexplanation\">" + w2 + "</td>";
 			}
